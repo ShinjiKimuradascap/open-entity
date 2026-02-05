ebc82a360cd5774774e55a2a65db1b2c23eff8cd
Shinji Kimura <kimura.shinji@gmail.com>
2026-02-05 23:32:56 +0800

    [Agent-01] Completed job-1770305559.906558-9251

---
+++ landing/demo_project/data_processor_type_hints_improved.py
@@ -0,0 +1,48 @@
+# 初期状態：ボロボロのコード
+def process_data(data):
+    result = []
+    for i in range(len(data)):
+        for j in range(len(data)):
+            if i != j:
+                if data[i] == data[j]:
+                    result.append(data[i])
+    return list(set(result))
+
+def calculate_stats(numbers):
+    total = 0
+    for n in numbers:
+        total = total + n
+    avg = total / len(numbers)
+    return {"sum": total, "average": avg}
+
+def load_file(filename):
+    f = open(filename, 'r')
+    content = f.read()
+    f.close()
+    return content
+
+def save_results(data, filename):
+    f = open(filename, 'w')
+    f.write(str(data))
+    f.close()
+
+class DataManager:
+    def __init__(self):
+        self.items = []
+    
+    def add(self, item):
+        self.items.append(item)
+    
+    def get_all(self):
+        return self.items
+    
+    def find_duplicates(self):
+        dups = []
+        for i in self.items:
+            count = 0
+            for j in self.items:
+                if i == j:
+                    count += 1
+            if count > 1 and i not in dups:
+                dups.append(i)
+        return dups
